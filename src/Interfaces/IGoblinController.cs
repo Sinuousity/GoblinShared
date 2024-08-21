7d29c4392778f24fcc52afe93366d3dc0b2ab118
Thomas <thomas.ir.rasor@gmail.com>
2024-08-21 10:50:51 -0500

    - added IsValid and IsCreated to IGoblinController

--- src/Interfaces/IGoblinController.cs
+++ src/Interfaces/IGoblinController.cs
@@ -5,6 +5,8 @@ namespace GoblinShared
 	/// <summary> Interface for the Goblin character root controller </summary>
 	public interface IGoblinController
 	{
+		bool IsValid { get; }
+		bool IsCreated { get; }
 		GoblinCustomisationDefinition customisationDefinition { get; }
 		Animator bodyAnimator { get; }
 	}
